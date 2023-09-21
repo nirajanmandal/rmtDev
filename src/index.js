72a274f8ba83c9c05676af92b7c59afa16c207cc
nirajanmandal <officialnirajan@gmail.com>
2023-09-21 23:31:31 +0545

    added webpack, babel, postcss

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './components/Error.js'
+import './components/JobList.js'
+import './components/Search.js'
+import './components/Spinner.js'
+import './components/Sorting.js'
+import './components/Pagination.js'
+import './components/Router.js'
+import './components/Bookmarks.js'
+import './components/Storage.js'
+
+import './index.css'
+
